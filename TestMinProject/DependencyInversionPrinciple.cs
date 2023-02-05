5244606d3e304d9331769b21ff13192ddb417cbe
Mrmeysam2 <mr.meysam2@gmail.com>
2023-02-05 15:35:10 +0330

    Create Solid Example

---
+++ TestMinProject/DependencyInversionPrinciple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMinProject
+{
+    public class DependencyInversionPrinciple
+    {
+        public static void Main()
+        {
+            Console.WriteLine("DependencyInversionPrinciple : اصل وارونگی وابستگی");
+        }
+    }
+}
---
+++ TestMinProject/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMinProject
+{
+    public class InterfaceSegregationPrinciple
+    {
+        public static void Main()
+        {
+            Console.WriteLine("InterfaceSegregationPrinciple : اصل جداسازی رابط");
+        }
+    }
+}

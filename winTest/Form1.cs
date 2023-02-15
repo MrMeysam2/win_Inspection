6cbb0dee0e1b3169fe8496e7dae480a7699dbe9b
Mrmeysam2 <mr.meysam2@gmail.com>
2023-02-15 11:33:37 +0330

    project wintest

---
+++ winTest/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winTest
+{
+    public partial class Form1 : Form
+    {
+        string Fname = "shagha";
+        string Lname = "sanati";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=test;User ID=sa;Password=Aa@135642");
+            SqlCommand cmd = new SqlCommand("insert into users (Fname,Lname) values (@fname,@lname)", con);
+            cmd.Parameters.AddWithValue("@fname", Fname);
+            cmd.Parameters.AddWithValue("@lname", Lname);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
